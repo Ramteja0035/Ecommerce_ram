6cc029348765876fdd94af196a1f9347cf0e8de0
Ramteja0035 <133530362+Ramteja0035@users.noreply.github.com>
2024-01-26 18:18:29 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,5 @@
 import JWT from "jsonwebtoken";
 import userModel from "../models/userModel.js";
-
-//Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
   try {
     const decode = JWT.verify(
